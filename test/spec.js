f837ec0fa035262aebeae14317ebf57803bdeb82
Sergey Kudashev <kudashevs@gmail.com>
2022-01-07 20:24:53 +0300

    Add tests add first basic case example.

---
+++ test/spec.js
@@ -0,0 +1,26 @@
+const testCompiler = require('./test-compiler.js');
+const assert = require('assert');
+
+const EXPECTED_OUTPUT_BASIC_CASE = `
+/* this comment should not be removed */
+module.exports = function addOne(num) {
+    const one = 1;
+    return num + one;
+}`;
+
+describe('basic case with object parameter', () => {
+    it('removes the appropriate block and leaves other code unchanged', async () => {
+        const stats = await testCompiler('fixtures/basic-case', {
+            options: {
+                'blocks': [
+                    {
+                        block: 'develblock',
+                        start: '/*',
+                        end: '*/'
+                    }]
+            }
+        });
+        const output = stats.toJson().modules[0].source;
+        assert.equal(output, EXPECTED_OUTPUT_BASIC_CASE);
+    });
+});
