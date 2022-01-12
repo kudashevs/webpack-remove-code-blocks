fab761a2232f4b194b42c64a246e76beb66fa062
Sergey Kudashev <kudashevs@gmail.com>
2022-01-12 15:36:42 +0300

    Refactor loader remove no longer used if conditional.

--- src/index.js
+++ src/index.js
@@ -31,26 +31,25 @@ function regexEscape(str) {
  */
 function RemoveCodeBlocksLoader(content) {
   const options = loaderUtils.getOptions(this) ?? defaultOptions;
-  if (options && options.blocks) {
-    options.blocks.forEach(function (block) {
-      if (typeof block === 'string') {
-        block = {
-          start: block + ':start',
-          end: block + ':end',
-          prefix: '/*',
-          suffix: '*/',
-        };
-      }
-      let prefix = block.prefix ? regexEscape(block.prefix) : '';
-      let suffix = block.suffix ? regexEscape(block.suffix) : '';
-      // prettier-ignore
-      let regex = new RegExp(
-          '[\\t ]*' + prefix + ' ?' + block.start + ' ?' + suffix + '[\\s\\S]*?' + prefix + ' ?' + block.end + ' ?' + suffix + '([\\t ]*\\n)?',
-          'g'
-      );
-      content = content.replace(regex, '');
-    });
-  }
+
+  options.blocks.forEach(function (block) {
+    if (typeof block === 'string') {
+      block = {
+        start: block + ':start',
+        end: block + ':end',
+        prefix: '/*',
+        suffix: '*/',
+      };
+    }
+    let prefix = block.prefix ? regexEscape(block.prefix) : '';
+    let suffix = block.suffix ? regexEscape(block.suffix) : '';
+    // prettier-ignore
+    let regex = new RegExp(
+      '[\\t ]*' + prefix + ' ?' + block.start + ' ?' + suffix + '[\\s\\S]*?' + prefix + ' ?' + block.end + ' ?' + suffix + '([\\t ]*\\n)?',
+      'g'
+    );
+    content = content.replace(regex, '');
+  });
 
   if (this.cacheable) {
     this.cacheable(true);
