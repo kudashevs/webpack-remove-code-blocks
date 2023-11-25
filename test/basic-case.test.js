e0c7fe69c2b4eeb92518b8383495a08cdbbdd760
Sergey Kudashev <kudashevs@gmail.com>
2023-11-26 00:29:58 +0300

    Refactor tests update tests in the basic-case.test file

--- test/basic-case.test.js
+++ test/basic-case.test.js
@@ -1,50 +1,53 @@
-const testCompiler = require("./test-compiler.js");
-const assert = require("assert");
-const EXPECTED_OUTPUT_BASIC_CASE = `
+const testCompiler = require('./test-compiler.js');
+const assert = require('assert');
+
+describe('a test suite for the simple/basic case', () => {
+  const EXPECTED_OUTPUT_BASIC_CASE = `
 /* this comment should not be removed */
 module.exports = function addOne(num) {
     const one = 1;
     return num + one;
 }`;
 
-describe('a basic case with no options', () => {
-    it('removes the appropriate block and leaves other code unchanged', async () => {
-        const stats = await testCompiler('fixtures/basic-case.js', {});
-        const output = stats.toJson({ source: true }).modules[0].source;
+  describe('a basic case used with no options', () => {
+    it('can remove the marked block and leave other code unchanged', async () => {
+      const stats = await testCompiler('fixtures/basic-case.js', {});
+      const output = stats.toJson({ source: true }).modules[0].source;
 
-        assert.equal(output, EXPECTED_OUTPUT_BASIC_CASE);
+      assert.equal(output, EXPECTED_OUTPUT_BASIC_CASE);
     });
-});
+  });
 
-describe('a basic case with a string parameter', () => {
-    it('removes the appropriate block and leaves other code unchanged', async () => {
-        const stats = await testCompiler('fixtures/basic-case.js', {
-            options: {
-                blocks: ['devblock'],
+  describe('a basic case used with a string parameter', () => {
+    it('can remove the marked block and leave other code unchanged', async () => {
+      const stats = await testCompiler('fixtures/basic-case.js', {
+        options: {
+          blocks: ['devblock'],
+        },
+      });
+      const output = stats.toJson({ source: true }).modules[0].source;
+
+      assert.equal(output, EXPECTED_OUTPUT_BASIC_CASE);
+    });
+  });
+
+  describe('a basic case used with an object parameter', () => {
+    it('can remove the marked block and leave other code unchanged', async () => {
+      const stats = await testCompiler('fixtures/basic-case.js', {
+        options: {
+          blocks: [
+            {
+              start: 'devblock:start',
+              end: 'devblock:end',
+              prefix: '/*',
+              suffix: '*/',
             },
-        });
-        const output = stats.toJson({ source: true }).modules[0].source;
+          ],
+        },
+      });
+      const output = stats.toJson({ source: true }).modules[0].source;
 
-        assert.equal(output, EXPECTED_OUTPUT_BASIC_CASE);
+      assert.equal(output, EXPECTED_OUTPUT_BASIC_CASE);
     });
-});
-
-describe('a basic case with an object parameter', () => {
-    it('removes the appropriate block and leaves other code unchanged', async () => {
-        const stats = await testCompiler('fixtures/basic-case.js', {
-            options: {
-                blocks: [
-                    {
-                        start: 'devblock:start',
-                        end: 'devblock:end',
-                        prefix: '/*',
-                        suffix: '*/',
-                    },
-                ],
-            },
-        });
-        const output = stats.toJson({ source: true }).modules[0].source;
-
-        assert.equal(output, EXPECTED_OUTPUT_BASIC_CASE);
-    });
+  });
 });
