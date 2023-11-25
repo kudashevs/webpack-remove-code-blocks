4547746ec6b8b99727a34e27dfcd8c893487df8e
Sergey Kudashev <kudashevs@gmail.com>
2023-11-26 01:20:24 +0300

    Refactor tests update tests in the compound-case.test file

--- test/compound-case.test.js
+++ test/compound-case.test.js
@@ -1,7 +1,7 @@
 const testCompiler = require('./test-compiler.js');
 const assert = require('assert');
 
-describe('a compound case from the README.md file', () => {
+describe('test suite for the compound case', () => {
   const EXPECTED_OUTPUT_COMPOUND_CASE = `
 var makeFoo = function(bar, baz) {
     // The following code will be removed with the loader
@@ -11,23 +11,25 @@ var makeFoo = function(bar, baz) {
     return new Foo(bar, baz);
 }`;
 
-  it('removes the appropriate block and leaves other code unchanged', async () => {
-    const stats = await testCompiler('fixtures/compound-case', {
-      options: {
-        blocks: [
-          'debug',
-          'devblock',
-          {
-            start: 'devblock_start',
-            end: 'devblock_end',
-            prefix: '/*',
-            suffix: '*/',
-          },
-        ],
-      },
-    });
-    const output = stats.toJson({ source: true }).modules[0].source;
+  describe('a compound case from the README.md file', () => {
+    it('can remove the marked block and leave other code unchanged', async () => {
+      const stats = await testCompiler('fixtures/compound-case', {
+        options: {
+          blocks: [
+            'debug',
+            'devblock',
+            {
+              start: 'devblock_start',
+              end: 'devblock_end',
+              prefix: '/*',
+              suffix: '*/',
+            },
+          ],
+        },
+      });
+      const output = stats.toJson({ source: true }).modules[0].source;
 
-    assert.equal(output, EXPECTED_OUTPUT_COMPOUND_CASE);
+      assert.equal(output, EXPECTED_OUTPUT_COMPOUND_CASE);
+    });
   });
 });
