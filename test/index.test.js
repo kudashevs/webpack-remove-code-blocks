f2f841cc75a02c08021a68fdd97c47b4265bcfe0
Sergey Kudashev <kudashevs@gmail.com>
2023-11-26 00:25:01 +0300

    Refactor index.test rename the testLoader constant

--- test/index.test.js
+++ test/index.test.js
@@ -1,19 +1,19 @@
 const assert = require('assert');
 
 describe('a default test suite', () => {
-  const testLoader = require('../src/index');
+  const loader = require('../src/index');
 
   it('can remove a block of code marked in the lower case', () => {
     let input = 'visible /* devblock:start */ will be removed /* devblock:end */';
     let expected = 'visible';
 
-    assert.equal(testLoader.call({}, input), expected);
+    assert.equal(loader.call({}, input), expected);
   })
 
   it('cannot remove a block of code marked in the upper case', () => {
     let input = 'visible /* DEVBLOCK:START */ won\'t be removed /* DEVBLOCK:END */';
     let expected = 'visible /* DEVBLOCK:START */ won\'t be removed /* DEVBLOCK:END */';
 
-    assert.equal(testLoader.call({}, input), expected);
+    assert.equal(loader.call({}, input), expected);
   })
 });
